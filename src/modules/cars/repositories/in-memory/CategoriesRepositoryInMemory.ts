6417490ec794df5c5730a1b4deeb279428cc2f8c
lfernandocosta <fernandocostadev98@gmail.com>
2022-08-24 14:21:09 -0300

    chore: create categories repository inmemory to apply tests

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,30 @@
+import { Category } from "../../entities/Category";
+import { ICreateCategoryDTO } from "../dtos/ICreateCategoryDTO";
+import { ICategoriesRepository } from "../ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  private categories: Category[] = [];
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = await new Category();
+
+    Object.assign(category, {
+      name: name,
+      description: description,
+    });
+
+    this.categories.push(category);
+  }
+  async findByName(name: string): Promise<Category> {
+    const category = await this.categories.find(
+      (category) => category.name === name
+    );
+    return category;
+  }
+  async list(): Promise<Category[]> {
+    const categories = await this.categories;
+    return categories;
+  }
+}
+
+export { CategoriesRepositoryInMemory };
